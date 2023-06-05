acebfa01caa7013ae6537ea6817a6901cda7b55a
traynhamdavid426 <traynhamdavid426@gmail.com>
2023-06-05 14:37:32 -0400

    New changes to map layout and sheltes added to map

--- src/SubComponents/Age/Mature.js
+++ src/SubComponents/Age/Mature.js
@@ -3,20 +3,133 @@ import { useNavigate } from "react-router-dom";
 
 const Mature =() => {
     let navigate= useNavigate()
-    return(<div>
-    Temperament: mature  Diet: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    Special care: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    Pros: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    cons: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    Diet: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    Special care: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    Pros: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-    cons: Suspendisse in est ante in nibh. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Cras pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo integer malesuada nunc vel risus commodo viverra.
-
-    <div>
+    return(<div className="container">
+    <div className="contents">
+    <h1 className="tc">Mature</h1>
+      <p>
+        Temperament: Diet: Suspendisse in est ante in nibh. A condimentum
+        vitae sapien pellentesque habitant morbi tristique senectus. Cras
+        pulvinar mattis nunc sed blandit. Augue eget arcu dictum varius.
+        Faucibus scelerisque eleifend donec pretium vulputate sapien nec
+        sagittis. Etiam erat velit scelerisque in dictum non consectetur.
+        Morbi leo urna molestie at elementum. Blandit cursus risus at ultrices
+        mi tempus imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor
+        aliquam nulla facilisi cras fermentum odio. At quis risus sed
+        vulputate. Lobortis mattis aliquam faucibus purus in massa.
+        Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Ut
+        sem viverra aliquet eget. Malesuada nunc vel risus commodo viverra
+        maecenas. Ultricies leo integer malesuada nunc vel risus commodo
+        viverra.
+      </p>
+      <p>
+        Special care: Suspendisse in est ante in nibh. A condimentum vitae
+        sapien pellentesque habitant morbi tristique senectus. Cras pulvinar
+        mattis nunc sed blandit. Augue eget arcu dictum varius. Faucibus
+        scelerisque eleifend donec pretium vulputate sapien nec sagittis.
+        Etiam erat velit scelerisque in dictum non consectetur. Morbi leo urna
+        molestie at elementum. Blandit cursus risus at ultrices mi tempus
+        imperdiet. Justo nec ultrices dui sapien. Tincidunt tortor aliquam
+        nulla facilisi cras fermentum odio. At quis risus sed vulputate.
+        Lobortis mattis aliquam faucibus purus in massa. Suspendisse potenti
+        nullam ac tortor vitae purus faucibus ornare. Ut sem viverra aliquet
+        eget. Malesuada nunc vel risus commodo viverra maecenas. Ultricies leo
+        integer malesuada nunc vel risus commodo viverra.
+      </p>
+      <p>
+        Pros: Suspendisse in est ante in nibh. A condimentum vitae sapien
+        pellentesque habitant morbi tristique senectus. Cras pulvinar mattis
+        nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque
+        eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit
+        scelerisque in dictum non consectetur. Morbi leo urna molestie at
+        elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo
+        nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras
+        fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam
+        faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae
+        purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc
+        vel risus commodo viverra maecenas. Ultricies leo integer malesuada
+        nunc vel risus commodo viverra.
+      </p>
+      <p>
+        cons: Suspendisse in est ante in nibh. A condimentum vitae sapien
+        pellentesque habitant morbi tristique senectus. Cras pulvinar mattis
+        nunc sed blandit. Augue eget arcu dictum varius. Faucibus scelerisque
+        eleifend donec pretium vulputate sapien nec sagittis. Etiam erat velit
+        scelerisque in dictum non consectetur. Morbi leo urna molestie at
+        elementum. Blandit cursus risus at ultrices mi tempus imperdiet. Justo
+        nec ultrices dui sapien. Tincidunt tortor aliquam nulla facilisi cras
+        fermentum odio. At quis risus sed vulputate. Lobortis mattis aliquam
+        faucibus purus in massa. Suspendisse potenti nullam ac tortor vitae
+        purus faucibus ornare. Ut sem viverra aliquet eget. Malesuada nunc
+        vel risus commodo viverra maecenas. Ultricies leo integer malesuada
+        nunc vel risus commodo viverra.
+      </p>
+      <p>
+        Diet: Egestas integer eget aliquet nibh praesent tristique magna sit
+        amet. Ullamcorper a lacus vestibulum sed arcu non odio euismod.
+        Dignissim suspendisse in est ante in nibh mauris. Suspendisse sed nisi
+        lacus sed viverra. Nulla facilisi etiam dignissim diam quis enim
+        lobortis scelerisque fermentum. Sagittis purus sit amet volutpat
+        consequat mauris nunc congue nisi. Morbi tristique senectus et netus
+        et malesuada fames ac turpis. Est lorem ipsum dolor sit amet.
+        Venenatis tellus in metus vulputate eu scelerisque felis. Massa
+        tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin.
+        Donec pretium vulputate sapien nec sagittis aliquam. Elementum tempus
+        egestas sed sed. Sed adipiscing diam donec adipiscing tristique risus
+        nec feugiat. A iaculis at erat pellentesque adipiscing. Sit amet dictum
+        sit amet. Odio euismod lacinia at quis risus sed vulputate odio ut.
+        Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.
+      </p>
+      <p>
+        Special care: Egestas integer eget aliquet nibh praesent tristique
+        magna sit amet. Ullamcorper a lacus vestibulum sed arcu non odio
+        euismod. Dignissim suspendisse in est ante in nibh mauris. Suspendisse
+        sed nisi lacus sed viverra. Nulla facilisi etiam dignissim diam quis
+        enim lobortis scelerisque fermentum. Sagittis purus sit amet volutpat
+        consequat mauris nunc congue nisi. Morbi tristique senectus et netus
+        et malesuada fames ac turpis. Est lorem ipsum dolor sit amet.
+        Venenatis tellus in metus vulputate eu scelerisque felis. Massa
+        tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin.
+        Donec pretium vulputate sapien nec sagittis aliquam. Elementum tempus
+        egestas sed sed. Sed adipiscing diam donec adipiscing tristique risus
+        nec feugiat. A iaculis at erat pellentesque adipiscing. Sit amet dictum
+        sit amet. Odio euismod lacinia at quis risus sed vulputate odio ut.
+        Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.
+      </p>
+      <p>
+        Pros: Egestas integer eget aliquet nibh praesent tristique magna sit
+        amet. Ullamcorper a lacus vestibulum sed arcu non odio euismod.
+        Dignissim suspendisse in est ante in nibh mauris. Suspendisse sed nisi
+        lacus sed viverra. Nulla facilisi etiam dignissim diam quis enim
+        lobortis scelerisque fermentum. Sagittis purus sit amet volutpat
+        consequat mauris nunc congue nisi. Morbi tristique senectus et netus
+        et malesuada fames ac turpis. Est lorem ipsum dolor sit amet.
+        Venenatis tellus in metus vulputate eu scelerisque felis. Massa
+        tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin.
+        Donec pretium vulputate sapien nec sagittis aliquam. Elementum tempus
+        egestas sed sed. Sed adipiscing diam donec adipiscing tristique risus
+        nec feugiat. A iaculis at erat pellentesque adipiscing. Sit amet dictum
+        sit amet. Odio euismod lacinia at quis risus sed vulputate odio ut.
+        Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.
+      </p>
+      <p>
+        cons: Egestas integer eget aliquet nibh praesent tristique magna sit
+        amet. Ullamcorper a lacus vestibulum sed arcu non odio euismod.
+        Dignissim suspendisse in est ante in nibh mauris. Suspendisse sed nisi
+        lacus sed viverra. Nulla facilisi etiam dignissim diam quis enim
+        lobortis scelerisque fermentum. Sagittis purus sit amet volutpat
+        consequat mauris nunc congue nisi. Morbi tristique senectus et netus
+        et malesuada fames ac turpis. Est lorem ipsum dolor sit amet.
+        Venenatis tellus in metus vulputate eu scelerisque felis. Massa
+        tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin.
+        Donec pretium vulputate sapien nec sagittis aliquam. Elementum tempus
+        egestas sed sed. Sed adipiscing diam donec adipiscing tristique risus
+        nec feugiat. A iaculis at erat pellentesque adipiscing. Sit amet dictum
+        sit amet. Odio euismod lacinia at quis risus sed vulputate odio ut.
+        Velit aliquet sagittis id consectetur purus ut faucibus pulvinar.
+      </p>
 <button onClick={()=>{navigate('/learncats')}}>All catergories</button>
-<button onClick={()=>{navigate('/prime')}}>back</button>
-<button onClick={()=>{navigate('/senior')}}>Next</button>
+<button  className="ml3" onClick={()=>{navigate('/prime')}}>back</button>
+<button className="ml3" onClick={()=>{navigate('/senior')}}>Next</button>
 </div>
 </div>)}
 
--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,130 +1,122 @@
 import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from "@react-google-maps/api";
-import React,{ useState } from "react";
-import '../SubComponents/Gender/Gender.css'
+import React, { useState } from "react";
+import "../SubComponents/Gender/Gender.css";
 
-
-
-const  Maps =() => {
+const Maps = () => {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey:process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
 
   const Map = (props) => {
     const { isLoaded } = props;
     const [selectedMarker, setSelectedMarker] = useState("");
-  
+
     const containerStyle = {
       width: "70vw",
       height: "70vh",
-      
-      
+      margin: "0 auto",
     };
+
     const center = {
       lat: 38.895,
       lng: -77.03667,
     };
-  
+
     const markers = [
       {
-        name: "Los Angeles, California",
-        status: "has cats",
+        name: "Washington Animal Rescue League",
+        status: "Adoptable pets",
         location: {
-          lat: 38.915161,
-          lng: -77.430992,
+          lat: 38.917747,
+          lng: -77.028884,
         },
+        website: "https://www.warl.org/",
+        phoneNumber: "(202) 726-2556",
       },
       {
-        name: "New York, New York",
-        status: "no cats",
+        name: "Humane Rescue Alliance",
+        status: "Adoptable pets",
         location: {
-          lat: 38.897675,
-          lng: -77.036530,
+          lat: 38.907772,
+          lng: -76.973270,
         },
+        website: "https://www.humanerescuealliance.org/",
+        phoneNumber: "(202) 727-1000",
       },
       {
-        name: "Denver, Colorado",
-        status: "cats here",
+        name: "City Dogs Rescue & City Kitties",
+        status: "Adoptable pets",
         location: {
-          lat: 31.765900,
-          lng: -106.461098,
+          lat: 38.914200,
+          lng: -77.029900,
         },
+        website: "https://www.citydogsrescuedc.org/",
+        phoneNumber: "(202) 567-7364",
       },
-      {
-        name: "Chicago, Illinois",
-        status: "my cats",
-        location: {
-          lat: 38.937496,
-          lng: -77.088806,
-        },
-      },
-      {
-        name: "location-5",
-        status: "your cats",
-        location: {
-          lat: 38.939360,
-          lng: -77.091750,
-        },
-      },
+      
     ];
-  
-  
-    
-    
-  
-    
-  
     
     return (
       isLoaded && (
         <div className="map">
-        <div>
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            
-          >
-            {markers.map((marker) => {
-              return (
-                <div key={marker.name}>
-                  <Marker
-                    position={marker.location}
-                   
-                    onClick={() => {
-                      setSelectedMarker(marker);
-                    }}
-                  />
-  
-                </div>
-              );
-            })}
-            {selectedMarker && (
-              <InfoWindow
-                position={selectedMarker.location}
-                options={{
-                  pixelOffset: new window.google.maps.Size(0, -40),
-                }}
-              >
-                <div>
-                  <h1>location -{selectedMarker.name}</h1>
-                  <h1>status - {selectedMarker.status}</h1>
-                  <button onClick={() => setSelectedMarker("")}>close</button>
-                </div>
-              </InfoWindow>
-            )}
-  
-          </GoogleMap>
-        </div></div>
+          <div style={{ position: "relative" }}>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+            >
+              {markers.map((marker) => {
+                return (
+                  <div key={marker.name}>
+                    <Marker
+                      position={marker.location}
+                      onClick={() => {
+                        setSelectedMarker(marker);
+                      }}
+                    />
+                  </div>
+                );
+              })}
+              {selectedMarker && (
+                <InfoWindow
+                  position={selectedMarker.location}
+                  options={{
+                    pixelOffset: new window.google.maps.Size(0, -40),
+                  }}
+                >
+                  <div>
+                    <h1>location -{selectedMarker.name}</h1>
+                    <h1>status - {selectedMarker.status}</h1>
+                    <h2>-{selectedMarker.website}</h2>
+                    <h2>-{selectedMarker.phoneNumber}</h2>
+                    <button
+                      style={{
+                        backgroundColor: "blue",
+                        color: "white",
+                        padding: "10px 20px",
+                        border: "none",
+                        borderRadius: "5px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => setSelectedMarker("")}
+                    >
+                      close
+                    </button>
+                  </div>
+                </InfoWindow>
+              )}
+            </GoogleMap>
+          </div>
+        </div>
       )
     );
   };
 
   return (
-    <div className="App">
-      
-      <Map isLoaded={isLoaded}/>
-    </div>
+    <div className="mt6">
+      <Map isLoaded={isLoaded} /></div>
+    
   );
-}
+};
 
 export default Maps;
