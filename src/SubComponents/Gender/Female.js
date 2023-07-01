c6ed52a8314bbc546208dbe25ede02cab667ea67
traynhamdavid426 <traynhamdavid426@gmail.com>
2023-07-01 12:57:03 -0400

    New changes to paragraph placements in gender SubComponents

--- src/SubComponents/Gender/Female.js
+++ src/SubComponents/Gender/Female.js
@@ -5,24 +5,24 @@ import "../Gender/Gender.css";
 const Female = () => {
     let navigate = useNavigate();
     return (
-      <div className="container">
-        <div className="div-container">
+      <div className="container ">
+        <div className="div-container tc">
           <div className="button-box ">
           <button className="button" onClick={() => { navigate('/learncats') }}>All categories</button>
           <button className="button ml2" onClick={() => { navigate('/male') }}>Males</button>
          </div> 
            <h3 className="tc navy f3">Temperament:</h3>
-          <div className="div-container f3 navy fw8"> Females Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+          <div className=" tc f3 navy fw8"> Females Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
          <h3 className="tc navy f3">Special care:</h3> 
-          <div className="div-container f3 navy fw8">
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div className="  f3 navy fw8">
+           <div className=""> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div> 
           </div>
           <h3 className="tc navy f3">Pros:</h3>
-          <div className="div-container f3 navy fw8">
+          <div className=" tc  f3 navy fw8">
              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
           <h3 className="tc navy f3">Cons:</h3>
-          <div className="div-container f3 navy fw8">
+          <div className=" tc  f3 navy fw8">
              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
         </div>
